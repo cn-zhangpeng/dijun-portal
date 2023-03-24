fe51dab43b0bda79bb541b1f865c35cc28526b14
山海紫穹 <zp95sky@163.com>
2023-03-24 08:52:46 +0800

    [Feat]: 添加白板页面

--- src/pages/project/Kanban.vue
+++ src/pages/project/Kanban.vue
@@ -1,6 +1,140 @@
 <template>
-  看板
+  <div id="project-kanban">
+    <ul class="kanban">
+      <li class="kanban-item">
+        <div class="header">
+          <span class="name">待处理</span>
+          <button>...</button>
+        </div>
+        <button class="add">+</button>
+        <ul class="task">
+          <li class="task-item">
+            <span class="name">【信令后台】框架搭建</span>
+<!--            <span><img alt="头像" src="https://tcs.teambition.net/thumbnail/111t8f70348037f544d170620cc0b9202e9c/w/200/h/200"></span>-->
+          </li>
+          <li class="task-item">
+            <span class="name">【信令后台】业务相关表结构设计</span>
+          </li>
+        </ul>
+      </li>
+      <li class="kanban-item">
+        <div class="header">
+          <span class="name">待处理</span>
+          <button>...</button>
+        </div>
+        <button class="add">+</button>
+        <ul class="task">
+          <li class="task-item">
+            <span class="name">【信令后台】框架搭建</span>
+            <!--            <span><img alt="头像" src="https://tcs.teambition.net/thumbnail/111t8f70348037f544d170620cc0b9202e9c/w/200/h/200"></span>-->
+          </li>
+          <li class="task-item">
+            <span class="name">【信令后台】业务相关表结构设计</span>
+          </li>
+        </ul>
+      </li>
+      <li class="kanban-item">
+        <div class="header">
+          <span class="name">待处理</span>
+          <button>...</button>
+        </div>
+        <button class="add">+</button>
+        <ul class="task">
+          <li class="task-item">
+            <span class="name">【信令后台】框架搭建</span>
+            <!--            <span><img alt="头像" src="https://tcs.teambition.net/thumbnail/111t8f70348037f544d170620cc0b9202e9c/w/200/h/200"></span>-->
+          </li>
+          <li class="task-item">
+            <span class="name">【信令后台】业务相关表结构设计</span>
+          </li>
+        </ul>
+      </li>
+      <li class="kanban-item">
+        <div class="header">
+          <span class="name">待处理</span>
+          <button>...</button>
+        </div>
+        <button class="add">+</button>
+        <ul class="task">
+          <li class="task-item">
+            <span class="name">【信令后台】框架搭建</span>
+            <!--            <span><img alt="头像" src="https://tcs.teambition.net/thumbnail/111t8f70348037f544d170620cc0b9202e9c/w/200/h/200"></span>-->
+          </li>
+          <li class="task-item">
+            <span class="name">【信令后台】业务相关表结构设计</span>
+          </li>
+        </ul>
+      </li>
+      <li class="kanban-item">
+        <div class="header">
+          <span class="name">待处理</span>
+          <button>...</button>
+        </div>
+        <button class="add">+</button>
+        <ul class="task">
+          <li class="task-item">
+            <span class="name">【信令后台】框架搭建</span>
+            <!--            <span><img alt="头像" src="https://tcs.teambition.net/thumbnail/111t8f70348037f544d170620cc0b9202e9c/w/200/h/200"></span>-->
+          </li>
+          <li class="task-item">
+            <span class="name">【信令后台】业务相关表结构设计</span>
+          </li>
+        </ul>
+      </li>
+    </ul>
+  </div>
 </template>
 
 <style lang="scss">
+#project-kanban {
+  min-height: 300px;
+  padding: 20px;
+  background: #F7F7F7;
+}
+.kanban {
+  display: flex;
+  align-items: flex-start;
+  background: #F7F7F7;
+  .kanban-item {
+    width: 300px;
+    min-height: 100px;
+    padding: 12px;
+    flex-shrink: 0;
+    .header {
+      height: 40px;
+      .name {
+        font-weight: bold;
+        line-height: 40px;
+      }
+    }
+    .add {
+      width: 100%;
+      height: 30px;
+      background: #FFF;
+      border-radius: 4px;
+      box-shadow: 0 1px 2px 0 rgba(0,0,0,.1);
+      margin-bottom: 8px;
+    }
+  }
+}
+.task {
+  .task-item {
+    width: 100%;
+    height: 48px;
+    background: #FFF;
+    border-radius: 4px;
+    box-shadow: 0 1px 2px 0 rgba(0,0,0,.1);
+    margin-bottom: 8px;
+    .name {
+      line-height: 48px;
+    }
+    img {
+      width: 24px;
+      height: 24px;
+      border-radius: 50%;
+      vertical-align: middle;
+    }
+  }
+}
+
 </style>
